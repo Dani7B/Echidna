53c93fe08b3681c3ec06c2ad9758c7dede9cbc91
Dani7B <dan.morgantini@stud.uniroma3.it>
2013-11-26 22:29:01 +0100

    fixed indentation

--- community-interaction/landing-area/es-hadoop/src/main/java/it/cybion/es/hadoop/Extractor.java
+++ community-interaction/landing-area/es-hadoop/src/main/java/it/cybion/es/hadoop/Extractor.java
@@ -16,45 +16,46 @@ import org.apache.hadoop.mapreduce.Job;
 import org.elasticsearch.hadoop.mr.ESInputFormat;
 
 /**
+ * Simple job to export snapshots from Elasticsearch as sequence files.
+ * Each user id has an array of related snapshots.
 * @author Daniele Morgantini
 */
 public class Extractor {
                 
     public static void main(String[] args) throws HadoopException{
             
-            /* Job for exporting snapshots */
-            Configuration conf = new Configuration();
-            conf.set("es.resource", "profiles/snapshot/_search?q=*:*"); // replace this with the relevant query
-                
-            try {
-                        Job job = new Job(conf);
-                 job.setInputFormatClass(ESInputFormat.class);
-                 job.setJarByClass(Extractor.class);
-                 job.setJobName("Snapshots export");
-                 
-                 job.setOutputFormatClass(SequenceFileOutputFormat.class);
-                 FileOutputFormat.setOutputPath(job, new Path("snapshots"));
-                 
-                 job.setMapperClass(SnapshotExtractorMapper.class);
+	    /* Job for exporting snapshots */
+	    Configuration conf = new Configuration();
+	    conf.set("es.resource", "profiles/snapshot/_search?q=*:*"); // replace this with the relevant query
+	        
+	    try {
+	    	Job job = new Job(conf);
+            job.setInputFormatClass(ESInputFormat.class);
+            job.setJarByClass(Extractor.class);
+            job.setJobName("Snapshots export");
+             
+            job.setOutputFormatClass(SequenceFileOutputFormat.class);
+            FileOutputFormat.setOutputPath(job, new Path("snapshots"));
+             
+            job.setMapperClass(SnapshotExtractorMapper.class);
             job.setReducerClass(SnapshotExtractorReducer.class);
             
-                 job.setMapOutputKeyClass(LongWritable.class);
-                 job.setMapOutputValueClass(MapWritable.class);
-                 
-                 job.setOutputKeyClass(LongWritable.class);
-                 job.setOutputValueClass(MapWritableArrayWritable.class);
+            job.setMapOutputKeyClass(LongWritable.class);
+            job.setMapOutputValueClass(MapWritable.class);
+             
+            job.setOutputKeyClass(LongWritable.class);
+            job.setOutputValueClass(MapWritableArrayWritable.class);
         
-         long startTime = System.currentTimeMillis();                 
-                 job.waitForCompletion(true);
-         System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+            long startTime = System.currentTimeMillis();                 
+            job.waitForCompletion(true);
+            System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
                 
-                } catch (IOException e) {
-                        throw new HadoopException("Could not create job or completing it", e);
-                } catch (ClassNotFoundException e) {
-                         throw new HadoopException("Could not find ESInputFormat.class", e);
-                } catch (InterruptedException e) {
-                        throw new HadoopException("Been interrupted", e);
-                }
+            } catch (IOException e) {
+            	throw new HadoopException("Could not create job or completing it", e);
+            } catch (ClassNotFoundException e) {
+                throw new HadoopException("Could not find ESInputFormat.class", e);
+            } catch (InterruptedException e) {
+                throw new HadoopException("Been interrupted", e);
+            }
 	}
-
 }
--- community-interaction/landing-area/snapshots/src/main/java/it/cybion/snapshots/SnapshotWorkflow.java
+++ community-interaction/landing-area/snapshots/src/main/java/it/cybion/snapshots/SnapshotWorkflow.java
@@ -20,60 +20,63 @@ import org.apache.hadoop.mapreduce.Job;
 import it.cybion.communityInteraction.landingArea.util.HadoopException;
 
 /**
+ * Simple job workflow that consists in counting how many snapshots have been collected
+ * per language and per user.
 * @author Daniele Morgantini
 */
 public class SnapshotWorkflow {
                 
     public static void main(String[] args) throws HadoopException{
-            Configuration conf = new Configuration();
+    	
+    	Configuration conf = new Configuration();
             
-                try {
-                        Job languageCount = new Job(conf);
-                 languageCount.setInputFormatClass(SequenceFileInputFormat.class);
-                 languageCount.setJarByClass(SnapshotWorkflow.class);
-                 languageCount.setJobName("Language count");
+    	try {
+    		Job languageCount = new Job(conf);
+	        languageCount.setInputFormatClass(SequenceFileInputFormat.class);
+	        languageCount.setJarByClass(SnapshotWorkflow.class);
+	        languageCount.setJobName("Language count");
                  
-                 FileInputFormat.setInputPaths(languageCount, new Path("snapshots"));
-                 FileOutputFormat.setOutputPath(languageCount, new Path("language-count"));
-                 
-                 languageCount.setMapperClass(LanguageMapper.class);
-            languageCount.setReducerClass(LanguageReducer.class);
-            languageCount.setCombinerClass(LanguageReducer.class);
-            
-                 languageCount.setMapOutputKeyClass(Text.class);
-                 languageCount.setMapOutputValueClass(IntWritable.class);
-                 
-                 languageCount.setOutputKeyClass(Text.class);
-                 languageCount.setOutputValueClass(IntWritable.class);
+	        FileInputFormat.setInputPaths(languageCount, new Path("snapshots"));
+	        FileOutputFormat.setOutputPath(languageCount, new Path("language-count"));
         
-                 languageCount.waitForCompletion(true);
-                 
-                 Job snapshotCount = new Job(conf);
-                 snapshotCount.setInputFormatClass(SequenceFileInputFormat.class);
-                 snapshotCount.setJarByClass(SnapshotWorkflow.class);
-                 snapshotCount.setJobName("Snapshot count");
-                 
-                 FileInputFormat.setInputPaths(snapshotCount, new Path("snapshots"));
-                 FileOutputFormat.setOutputPath(snapshotCount, new Path("snapshot-count"));
-                 
-                 snapshotCount.setMapperClass(UserMapper.class);
-                 snapshotCount.setReducerClass(UserReducer.class);
-                 snapshotCount.setCombinerClass(UserReducer.class);
-            
-                 snapshotCount.setMapOutputKeyClass(LongWritable.class);
-                 snapshotCount.setMapOutputValueClass(IntWritable.class);
-                 
-                 snapshotCount.setOutputKeyClass(LongWritable.class);
-                 snapshotCount.setOutputValueClass(IntWritable.class);
-        
-                 snapshotCount.waitForCompletion(true);
-                
-                } catch (IOException e) {
-                        throw new HadoopException("Could not create job or completing it", e);
-                } catch (ClassNotFoundException e) {
-                         throw new HadoopException("Could not find SequenceFileInputFormat.class", e);
-                } catch (InterruptedException e) {
-                        throw new HadoopException("Been interrupted", e);
-                }
+        	languageCount.setMapperClass(LanguageMapper.class);
+        	languageCount.setReducerClass(LanguageReducer.class);
+        	languageCount.setCombinerClass(LanguageReducer.class);
+    
+		    languageCount.setMapOutputKeyClass(Text.class);
+		    languageCount.setMapOutputValueClass(IntWritable.class);
+		     
+		    languageCount.setOutputKeyClass(Text.class);
+		    languageCount.setOutputValueClass(IntWritable.class);
+		
+		    languageCount.waitForCompletion(true);
+		    
+		    Job snapshotCount = new Job(conf);
+		    snapshotCount.setInputFormatClass(SequenceFileInputFormat.class);
+		    snapshotCount.setJarByClass(SnapshotWorkflow.class);
+		    snapshotCount.setJobName("Snapshot count");
+		    
+		    FileInputFormat.setInputPaths(snapshotCount, new Path("snapshots"));
+		    FileOutputFormat.setOutputPath(snapshotCount, new Path("snapshot-count"));
+		    
+		    snapshotCount.setMapperClass(UserMapper.class);
+		    snapshotCount.setReducerClass(UserReducer.class);
+		    snapshotCount.setCombinerClass(UserReducer.class);
+		    
+		    snapshotCount.setMapOutputKeyClass(LongWritable.class);
+		    snapshotCount.setMapOutputValueClass(IntWritable.class);
+		     
+		    snapshotCount.setOutputKeyClass(LongWritable.class);
+		    snapshotCount.setOutputValueClass(IntWritable.class);
+		
+		    snapshotCount.waitForCompletion(true);
+		    
+		    } catch (IOException e) {
+		            throw new HadoopException("Could not create job or completing it", e);
+		    } catch (ClassNotFoundException e) {
+		             throw new HadoopException("Could not find SequenceFileInputFormat.class", e);
+		    } catch (InterruptedException e) {
+		            throw new HadoopException("Been interrupted", e);
+		    }
     }
 }
