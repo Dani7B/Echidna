1adf37bf0d701fec178c242de32f7e6079b1ef9c
Dani7B <dan.morgantini@stud.uniroma3.it>
2013-11-28 13:28:15 +0100

    #2 developed and tested single put

--- community-interaction/serving-layer/hbase/src/main/java/hbase/HBaseClient.java
+++ community-interaction/serving-layer/hbase/src/main/java/hbase/HBaseClient.java
@@ -1,6 +1,8 @@
 package hbase;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -8,10 +10,14 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HConnection;
 import org.apache.hadoop.hbase.client.HConnectionManager;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -83,4 +89,36 @@ public class HBaseClient {
 		return null;
 	}
 
+	public void put(HTable table, String row, String colfam, String col,
+					long ts, String value) throws IOException {
+		
+		Put tableRow = new Put(Bytes.toBytes(row));
+		
+		tableRow.add(Bytes.toBytes(colfam), Bytes.toBytes(col), ts,
+	            Bytes.toBytes(value));
+		table.put(tableRow);		
+	}
+	
+	/***/
+	public void put(HTable table, String[] rows, String[] colfams, String[] cols,
+            long[] tss, String[] values) throws IOException {
+		
+		List<Put> tableRows = new ArrayList<Put>();
+		for(int i=0; i<rows.length; i++) {
+			
+			Put tableRow = new Put(Bytes.toBytes(rows[i]));
+			tableRow.add(Bytes.toBytes(colfams[i]), Bytes.toBytes(cols[i]), tss[i],
+		            Bytes.toBytes(values[i]));
+			
+			tableRows.add(tableRow);
+		}
+		
+		table.put(tableRows);
+	}
+	
+	public Result get(HTable table, String row) throws IOException {
+		
+		Get tableRow = new Get(Bytes.toBytes(row));
+		return table.get(tableRow);
+	}
 }
--- community-interaction/serving-layer/hbase/src/test/java/hbase/HBaseClientTest.java
+++ community-interaction/serving-layer/hbase/src/test/java/hbase/HBaseClientTest.java
@@ -2,9 +2,8 @@ package hbase;
 
 import java.io.IOException;
 
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
@@ -14,6 +13,8 @@ import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 
 /**
  * Test class for HBaseClient
@@ -24,38 +25,65 @@ public class HBaseClientTest {
     
     private HBaseClient client;
     
+    private HTable testTable;
+    
     @BeforeClass
-    public void setUp() throws MasterNotRunningException, ZooKeeperConnectionException {
-    	
-    	this.client = new HBaseClient();
+    public void setUp() throws IOException {
+		this.client = new HBaseClient();
+
+    	if(this.client.existsTable("test"))
+    		this.client.deleteTable("test");
+    	this.client.createTable("test", "prova");
+    	this.testTable = this.client.getTable("test");
     }
     
     @Test
     public void hasCreatedTable() throws IOException {
     	
     	this.client.createTable("testing", "prova");
-    	
     	boolean value = this.client.existsTable("testing");
-    	
     	assertTrue(value);
     	
     	HTable table = this.client.getTable("testing");
-    	
     	assertNotNull(table);
-    	
     }
     
     @Test
     public void hasDeletedTable() throws IOException {
-    	
-    	this.client.deleteTable("testing");
-    	
-    	boolean value = this.client.existsTable("testing");
-
-    	assertFalse(value);
-    	
-    	HTable table = this.client.getTable("testing");
-    	
-    	assertNull(table);
+            
+            this.client.deleteTable("testing");
+            boolean value = this.client.existsTable("testing");
+            assertFalse(value);
+            
+            HTable table = this.client.getTable("testing");
+            assertNull(table);
     }
+    
+    @Test
+    public void hasInsertedRow() throws IOException {
+    	
+    	LOGGER.info("Auto flush: " + this.testTable.isAutoFlush());
+    	
+    	this.client.put(this.testTable, "row1", "prova", "col1", System.currentTimeMillis(), "50000");
+    	
+    	Result result = this.client.get(this.testTable, "row1");    	
+    	LOGGER.info(result.toString());
+    	assertEquals(result.size(),1);
+    	
+    	this.client.put(this.testTable, "row1", "prova", "col2", System.currentTimeMillis(), "51");
+    	result = this.client.get(this.testTable, "row1");    	
+    	LOGGER.info(result.toString());
+    	assertEquals(result.size(),2); //it's the size of the cell
+    	
+    	this.client.put(this.testTable, "row2", "prova", "col1", System.currentTimeMillis(), "16");
+    	result = this.client.get(this.testTable, "row2");    	
+    	LOGGER.info(result.toString());
+    	assertEquals(result.size(),1);
+    }
+    
+    @Test
+    public void hasInsertedRows() throws IOException {
+    	
+    }
+    
 }
