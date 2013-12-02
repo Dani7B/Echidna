9f176e46b9942372c99b3014b3bc502048ce6a0e
Dani7B <dan.morgantini@stud.uniroma3.it>
2013-12-02 14:18:33 +0100

    #4 added retrieve methods. More tests to implement

--- community-interaction/serving-layer/hbase/src/main/java/hbase/HBaseClient.java
+++ community-interaction/serving-layer/hbase/src/main/java/hbase/HBaseClient.java
@@ -124,14 +124,13 @@ public class HBaseClient {
 	 * @param colfam the name of the column family
 	 * @param col the column name
 	 * @param ts the timestamp
-	 * @param value the value */
+	 * @param value the value in byte */
 	public void put(HTable table, String row, String colfam, String col,
-					long ts, String value) throws IOException {
+					long ts, byte[] value) throws IOException {
 		
 		Put tableRow = new Put(Bytes.toBytes(row));
 		
-		tableRow.add(Bytes.toBytes(colfam), Bytes.toBytes(col), ts,
-	            Bytes.toBytes(value));
+		tableRow.add(Bytes.toBytes(colfam), Bytes.toBytes(col), ts, value);
 		table.put(tableRow);		
 	}
 	
@@ -145,14 +144,13 @@ public class HBaseClient {
 	 * @param tss the timestamps
 	 * @param values the values */
 	public void put(HTable table, String[] rows, String[] colfams, String[] cols,
-            long[] tss, String[] values) throws IOException {
+            long[] tss, byte[][] values) throws IOException {
 		
 		List<Put> tableRows = new ArrayList<Put>();
 		for(int i=0; i<rows.length; i++) {
 			
 			Put tableRow = new Put(Bytes.toBytes(rows[i]));
-			tableRow.add(Bytes.toBytes(colfams[i]), Bytes.toBytes(cols[i]), tss[i],
-		            Bytes.toBytes(values[i]));
+			tableRow.add(Bytes.toBytes(colfams[i]), Bytes.toBytes(cols[i]), tss[i], values[i]);
 			
 			tableRows.add(tableRow);
 		}
@@ -161,12 +159,157 @@ public class HBaseClient {
 	}
 	
 	/**
-	 * Return the result of the get query
+	 * @return the result of the get query
 	 * @param table the HTable to query 
 	 * @param row the id of the row */
 	public Result get(HTable table, String row) throws IOException {
 		
 		Get tableRow = new Get(Bytes.toBytes(row));
 		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return the result of the get query, filtered with the specified fields and time range
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 * @param timeRange the specified timestamp range: [timeRange[0], timeRange[1])
+	 * @param maxVersions the number of versions to retrieve
+	 * */
+	public Result get(HTable table, String row, String[] columnFamilies,
+				long[] timeRange, int maxVersions) throws IOException {
+		
+		Get tableRow = prepareGet(row, columnFamilies, 0, timeRange);
+		tableRow.setMaxVersions(maxVersions);
+		
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return the result of the get query, filtered with the specified fields and timestamp
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 * @param timeStamp the timeStamp identifying the version to retrieve
+	 * @param maxVersions the number of versions to retrieve
+	 */
+	public Result get(HTable table, String row, String[] columnFamilies,
+				long timeStamp, int maxVersions) throws IOException {
+		
+		Get tableRow = prepareGet(row, columnFamilies, timeStamp, null);
+		tableRow.setMaxVersions(maxVersions);
+		
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return a single result of the get query, filtered with the specified fields and time range
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 * @param timeRange the specified timestamp range: [timeRange[0], timeRange[1])
+	 **/
+	public Result get(HTable table, String row, String[] columnFamilies, long[] timeRange) throws IOException {
+		
+		Get tableRow = prepareGet(row, columnFamilies, 0, timeRange);
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return a single result of the get query, filtered with the specified fields and timestamp
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 * @param timeStamp the timeStamp identifying the version to retrieve
+	 **/
+	public Result get(HTable table, String row, String[] columnFamilies, long timeStamp) throws IOException {
+		
+		Get tableRow = prepareGet(row, columnFamilies, timeStamp, null);
+		
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return all the versions of the query result, filtered with the specified fields and time range
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 * @param timeRange the specified timestamp range: [timeRange[0], timeRange[1])
+	 **/
+	public Result getHistory(HTable table, String row, String[] columnFamilies, long[] timeRange) throws IOException {
+		
+		Get tableRow = prepareGet(row, columnFamilies, 0, timeRange);
+		tableRow.setMaxVersions();
+		
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return all the versions of the query result, filtered with the specified fields and timestamp
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 * @param timeStamp the timeStamp identifying the version to retrieve
+	 */
+	public Result getHistory(HTable table, String row, String[] columnFamilies, long timeStamp) throws IOException {
+		
+		Get tableRow = prepareGet(row, columnFamilies, timeStamp, null);
+		tableRow.setMaxVersions();
+		
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return all the versions of the query result, filtered with the specified fields
+	 * @param table the HTable to query 
+	 * @param row the id of the row 
+	 * @param columnFamilies the column families names
+	 */
+	public Result getHistory(HTable table, String row, String[] columnFamilies) throws IOException {
+		
+		Get tableRow = prepareGetHistory(row, columnFamilies);
+		
+		return table.get(tableRow);
+	}
+	
+	/**
+	 * @return the get query to be executed
+	 * @param row the id of the row
+	 * @param columnFamilies the column families names
+	 * @param timeRange the specified timestamp range: [timeRange[0], timeRange[1])
+	 */
+	private Get prepareGet(String row, String[] columnFamilies,	long timeStamp, long[] timeRange) throws IOException {
+		
+		Get get = new Get(Bytes.toBytes(row));
+		
+		if(columnFamilies != null)
+			for(String cf : columnFamilies)
+				get.addFamily(Bytes.toBytes(cf));
+		
+		if(timeRange == null)
+			get.setTimeStamp(timeStamp);
+		else
+			get.setTimeRange(timeRange[0], timeRange[1]);
+		
+		return get;
+	}
+	
+	/**
+	 * @return the get query to be executed
+	 * @param row the id of the row
+	 * @param columnFamilies the column families names
+	 * @param timeRange the specified timestamp range: [timeRange[0], timeRange[1])
+	 */
+	private Get prepareGetHistory(String row, String[] columnFamilies) throws IOException {
+		
+		Get get = new Get(Bytes.toBytes(row));
+		
+		if(columnFamilies != null)
+			for(String cf : columnFamilies)
+				get.addFamily(Bytes.toBytes(cf));
+		
+		get.setMaxVersions();
+		
+		return get;
 	}
 }
--- community-interaction/serving-layer/hbase/src/test/java/hbase/HBaseClientTest.java
+++ community-interaction/serving-layer/hbase/src/test/java/hbase/HBaseClientTest.java
@@ -1,9 +1,13 @@
 package hbase;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeClass;
@@ -14,6 +18,7 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 
 /**
  * Test class for HBaseClient
@@ -30,12 +35,12 @@ public class HBaseClientTest {
     public void setUp() throws IOException {
 		this.client = new HBaseClient();
 
-    	if(this.client.existsTable("test"))
-    		this.client.deleteTable("test");
+    	if(this.client.existsTable("casa"))
+    		this.client.deleteTable("casa");
     	
-    	this.client.createTable("test", "prova", "try");
-    	this.testTable = this.client.getTable("test");
-    	this.testTable.setWriteBufferSize(20971520L); // It doesn't get the default value from config otherwise
+    	this.client.createTable("casa", "salone", "soggiorno");
+    	this.testTable = this.client.getTable("casa");
+    	this.testTable.setWriteBufferSize(20971520L); // Otherwise it doesn't get the default value from config
     }
     
     @Test
@@ -65,18 +70,18 @@ public class HBaseClientTest {
     	
     	LOGGER.info("Auto flush: " + this.testTable.isAutoFlush());
     	
-    	this.client.put(this.testTable, "row1", "prova", "col1", System.currentTimeMillis(), "50000");
+    	this.client.put(this.testTable, "row1", "salone", "mq", System.currentTimeMillis(), Bytes.toBytes(25));
     	
     	Result result = this.client.get(this.testTable, "row1");    	
     	LOGGER.info(result.toString());
     	assertEquals(result.size(),1);
     	
-    	this.client.put(this.testTable, "row1", "prova", "col2", System.currentTimeMillis(), "51");
+    	this.client.put(this.testTable, "row1", "soggiorno", "luminoso", System.currentTimeMillis(), Bytes.toBytes(true));
     	result = this.client.get(this.testTable, "row1");    	
     	LOGGER.info(result.toString());
     	assertEquals(result.size(),2); //it's the size of the cell
     	
-    	this.client.put(this.testTable, "row2", "prova", "col1", System.currentTimeMillis(), "16");
+    	this.client.put(this.testTable, "row2", "soggiorno", "mq", System.currentTimeMillis(), Bytes.toBytes(16));
     	result = this.client.get(this.testTable, "row2");    	
     	LOGGER.info(result.toString());
     	assertEquals(result.size(),1);
@@ -88,13 +93,13 @@ public class HBaseClientTest {
     	this.testTable.setAutoFlush(false);
     	LOGGER.info("Auto flush: " + this.testTable.isAutoFlush());
     	
-    	this.client.put(this.testTable, "row3", "prova", "col1", System.currentTimeMillis(), "50");
+    	this.client.put(this.testTable, "row3", "soggiorno", "mq", System.currentTimeMillis(), Bytes.toBytes(15));
     	
     	Result result = this.client.get(this.testTable, "row3");    	
     	LOGGER.info(result.toString());
     	assertEquals(result.size(),0);
     	
-    	this.client.put(this.testTable, "row3", "prova", "col2", System.currentTimeMillis(), "51");
+    	this.client.put(this.testTable, "row3", "soggiorno", "luminoso", System.currentTimeMillis(), Bytes.toBytes(false));
     	this.testTable.flushCommits();
 
     	result = this.client.get(this.testTable, "row3");    	
@@ -107,10 +112,10 @@ public class HBaseClientTest {
     public void hasInsertedRows() throws IOException {
     	
     	String[] rows = {"row4", "row4"};
-    	String[] colfams = {"prova", "try"};
-    	String[] cols = {"col1", "col1"};
+    	String[] colfams = {"salone", "soggiorno"};
+    	String[] cols = {"mq", "mq"};
     	long[] tss = {1L, 2L};
-    	String[] values = {"10", "11"};
+    	byte[][] values = {Bytes.toBytes(10), Bytes.toBytes(11)};
     	this.client.put(this.testTable, rows, colfams, cols, tss, values);
     	
     	Result result = this.client.get(this.testTable, "row4");
@@ -121,6 +126,46 @@ public class HBaseClientTest {
     @Test
     public void checkWriteBufferSize() throws IOException {
     	assertEquals(this.testTable.getWriteBufferSize(),20971520L);
+    }
+    
+    @Test
+    public void getAllVersions() throws IOException {
+    	
+    	String[] rows = {"row6", "row6", "row6"};
+    	String[] colfams = {"salone", "soggiorno", "salone"};
+    	String[] cols = {"mq", "mq", "mq"};
+    	long[] tss = {1L, 2L, 3L};
+    	byte[][] values = {Bytes.toBytes(10), Bytes.toBytes(12), Bytes.toBytes(11)};
+    	this.client.put(this.testTable, rows, colfams, cols, tss, values);
+    	
+    	String[] columnFamilies = {"salone"};
+    	Result result = this.client.getHistory(this.testTable, "row6", columnFamilies);
+    	
+    	List<Integer> expected = new ArrayList<Integer>();
+    	expected.add(11);
+    	expected.add(10);
+
+    	List<Integer> resultMq = new ArrayList<Integer>();
+    	for(KeyValue kv : result.raw())
+    		resultMq.add(Bytes.toInt(kv.getValue()));
+    	
+    	assertEquals(expected, resultMq);
+    	
+    	expected.add(12);
+    	resultMq = new ArrayList<Integer>();
+    	result = this.client.getHistory(this.testTable, "row6", null);
+    	for(KeyValue kv : result.raw())
+    		resultMq.add(Bytes.toInt(kv.getValue()));
+    	
+    	assertEquals(expected, resultMq); // since we had all qualifiers with the same name
+    	
+    	this.client.put(this.testTable, "row6", "soggiorno", "valore", 4L, Bytes.toBytes(40));
+    	resultMq = new ArrayList<Integer>();
+    	result = this.client.getHistory(this.testTable, "row6", null);
+    	for(KeyValue kv : result.raw())
+    		resultMq.add(Bytes.toInt(kv.getValue()));
+    	
+    	assertNotEquals(expected, resultMq); // since it adds up 40 to the list
     }
     
 }
