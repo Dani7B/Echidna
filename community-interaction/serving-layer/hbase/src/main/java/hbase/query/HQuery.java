56fbbd23880aefd922639bb02cf01b2e1a3a325b
Dani7B <dan.morgantini@stud.uniroma3.it>
2013-12-21 09:37:37 +0100

    added comments to HQuery since I noticed they were missing when carrying out the demo

--- community-interaction/serving-layer/hbase/src/main/java/hbase/query/HQuery.java
+++ community-interaction/serving-layer/hbase/src/main/java/hbase/query/HQuery.java
@@ -6,34 +6,63 @@ import java.util.List;
 
 import hbase.query.subquery.HSubQuery;
 
+/**
+ * Simple class to represent a query
+ * @author Daniele Morgantini
+ */
 public class HQuery {
 	
 	private Authors users;
 	
 	private List<HSubQuery> subqueries;
-		
+	
+	/**
+	 * No argument consturctor
+	 */
 	public HQuery() {
 		this.users = new Authors(this);
 		this.subqueries = new ArrayList<HSubQuery>();
 	}
 	
+	/**
+	 * Retrieves the authors associated to the query
+	 * @return the authors associated to the query 
+	 */
 	public Authors users() {
 		return this.users;
 	}
 	
+	/**
+	 * Updates the authors associated to the query
+	 * @param authors the list of authors to set
+	 */
 	public void updateUsers(final List<Author> authors) {
 		this.users.setAuthors(authors);;
 	}
 	
+	
+	/**
+	 * Retrieves the subqueries the query is composed of
+	 * @return the list of subqueries the query is composed of
+	 */
 	public List<HSubQuery> getSubqueries() {
 		return this.subqueries;
 	}
 	
+	
+	/**
+	 * Adds a subquery to the subqueries the query is composed of
+	 * @param subquery the subquery to add
+	 */
 	public void addSubquery(final HSubQuery subquery) {
 		this.subqueries.add(subquery);
 	}
 	
 	
+	/**
+	 * Answers the query
+	 * @return the authors that match the query
+	 * */
 	public Authors answer() {
 		
 		for(HSubQuery s : this.subqueries)
