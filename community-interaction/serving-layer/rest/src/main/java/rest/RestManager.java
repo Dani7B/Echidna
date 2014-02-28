8e3594e7cbdb45a89b797266333767c487b86527
Dani7B <dan.morgantini@stud.uniroma3.it>
2014-02-28 17:42:07 +0100

    #16 improving the request syntax

--- community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
+++ community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
@@ -1,5 +1,8 @@
 package rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import hbase.query.HQuery;
 
 import javax.ws.rs.GET;
@@ -7,7 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
  
-@Path("/hello")
+@Path("/search")
 public class RestManager {
 	
 	private HQuery query;
@@ -19,11 +22,19 @@ public class RestManager {
 	@GET
 	@Path("/users")
 	public Response getMsg(
-		@QueryParam("msg") String msg ){
+		@QueryParam("tm_atLeast") int tm_al,
+		@QueryParam("tm_when") String tm_when,
+		@QueryParam("tm_users") String tm_users){
 			
-			String output = "Jersey says : " + msg;
-			return Response.status(200).entity(output).build();
+		List<Long> users = new ArrayList<Long>();
+		for(String s : tm_users.split(","))
+			users.add(Long.parseLong(s));
 		
+			String output = "Users that mentioned: " +
+			"at least: " + tm_al +
+			" when: " + tm_when +
+			" amongst: " + users;
+			return Response.status(200).entity(output).build();
 	}
  
 }
