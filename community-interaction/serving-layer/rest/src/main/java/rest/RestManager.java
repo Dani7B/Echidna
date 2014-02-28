ce279df4943d3c545e888a39748aff90213f7e24
Dani7B <dan.morgantini@stud.uniroma3.it>
2014-02-28 17:12:03 +0100

    #16 using query params

--- community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
+++ community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
@@ -1,21 +1,29 @@
 package rest;
 
+import hbase.query.HQuery;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
  
 @Path("/hello")
 public class RestManager {
+	
+	private HQuery query;
  
+	public RestManager() {
+		query = new HQuery();
+	}
+	
 	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
- 
-		String output = "Jersey says : " + msg;
- 
-		return Response.status(200).entity(output).build();
- 
+	@Path("/users")
+	public Response getMsg(
+		@QueryParam("msg") String msg ){
+			
+			String output = "Jersey says : " + msg;
+			return Response.status(200).entity(output).build();
+		
 	}
  
 }
