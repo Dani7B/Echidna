804524949f77f97501315b03741984359dd78f69
Dani7B <dan.morgantini@stud.uniroma3.it>
2014-02-28 18:04:59 +0100

    #16 actually working AuthorsThatMentionedQuery

--- community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
+++ community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
@@ -3,7 +3,12 @@ package rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import hbase.query.AtLeast;
+import hbase.query.Author;
+import hbase.query.Authors;
 import hbase.query.HQuery;
+import hbase.query.Mention;
+import hbase.query.time.LastMonth;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,10 +18,13 @@ import javax.ws.rs.core.Response;
 @Path("/search")
 public class RestManager {
 	
+	private Authors authors;
+	
 	private HQuery query;
  
 	public RestManager() {
-		query = new HQuery();
+		this.query = new HQuery();
+		this.authors = new Authors(query);
 	}
 	
 	@GET
@@ -26,15 +34,22 @@ public class RestManager {
 		@QueryParam("tm_when") String tm_when,
 		@QueryParam("tm_users") String tm_users){
 			
-		List<Long> users = new ArrayList<Long>();
+		List<Mention> users = new ArrayList<Mention>();
 		for(String s : tm_users.split(","))
-			users.add(Long.parseLong(s));
+			users.add(new Mention(Long.parseLong(s)));
+		Mention[] mentions = new Mention[3];
+		if(tm_when.equalsIgnoreCase("last_month"))
+			this.authors.thatMentioned(new LastMonth(), new AtLeast(tm_al), users.toArray(mentions));
+		this.authors = this.query.answer();
 		
-			String output = "Users that mentioned: " +
-			"at least: " + tm_al +
-			" when: " + tm_when +
-			" amongst: " + users;
-			return Response.status(200).entity(output).build();
+		List<Long> result = new ArrayList<Long>();
+		for(Author a : this.authors.getAuthors())
+			result.add(a.getId());
+		String output = "Users that mentioned: " +
+		"at least: " + tm_al +
+		" when: " + tm_when +
+		" amongst: " + result;
+		return Response.status(200).entity(output).build();
 	}
  
 }
