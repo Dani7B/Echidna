fe5e2a09c8de6b350cedb12b0366d3e980446221
Dani7B <dan.morgantini@stud.uniroma3.it>
2014-03-04 14:54:51 +0100

    #16 improved response messages

--- community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
+++ community-interaction/serving-layer/rest/src/main/java/rest/RestManager.java
@@ -32,45 +32,88 @@ public class RestManager {
 	
 	@GET
 	@Path("/users")
-	public Response getMsg(
-		@QueryParam("tm_atLeast") int tm_al,
+	public Response generic(
+		@DefaultValue("1") @QueryParam("tm_atLeast") int tm_al,
 		@QueryParam("tm_when") String tm_when,
 		@QueryParam("tm_users") String tm_users,
-		@QueryParam("wf_atLeast") int wf_al,
+		@DefaultValue("1") @QueryParam("wf_atLeast") int wf_al,
 		@QueryParam("wf_users") String wf_users,
-		@DefaultValue("true") @QueryParam("byId") boolean byId,
+		@QueryParam("wff_users") String wff_users,
+		@QueryParam("wfafb_users") String wfafb_users,
+		@DefaultValue("1") @QueryParam("wfm_atLeast") int wfm_al,
+		@QueryParam("wfm_when") String wfm_when,
+		@DefaultValue("1") @QueryParam("wfm_minTimes") int wfm_minTimes,
+		@QueryParam("wfm_users") String wfm_users,
+		@QueryParam("byId") String byId,
+		@DefaultValue("true") @QueryParam("byHits") boolean byHits,
 		@DefaultValue("10") @QueryParam("take") int take){
 		
+		String output = "Users";
 		if(tm_users!=null && tm_when!=null){
 			this.authorsThatMentionedSubquery(tm_al, tm_when, tm_users);
+			output += " that mentioned " +
+					"at least: " + tm_al + " user";
+			if(tm_al>1)
+				output += "s";
+			output += " when: " + tm_when +
+					" amongst: " + tm_users + ",";
 		}
 		
 		if(wf_users!=null){
 			this.authorsWhoFollowSubquery(wf_al, wf_users);
+			output += " who follow " +
+					"at least: " + wf_al + " user";
+			if(wf_al>1)
+				output += "s";
+			output += " amongst: " + wf_users + ",";
 		}
 		
-		this.authors.rankedById(byId);
+		if(wff_users!=null) {
+			this.authorsWhoseFollowersFollowSubquery(wff_users);
+			output += " whose followers follow one amongst " + wff_users + ",";
+		}
+		
+		if(wfafb_users!=null) {
+			this.authorsWhoseFollowersAreFollowedBySubquery(wfafb_users);
+			output += " whose followers are followed by one amongst " + wfafb_users + ",";
+		}
+		
+		if(wfm_users!=null) {
+			this.authorsWhoseFollowersMentionedSubquery(wfm_al, wfm_when, wfm_users, wfm_minTimes);
+			output += " whose followers mentioned " + 
+						" at least: " + wfm_al + " user";
+			if(wfm_al>1)
+				output += "s";
+			output += " when: " + wfm_when +
+						"amongst: " + wfm_users +
+						"exactly or more than " + wfm_minTimes + " time";
+			if(wfm_minTimes>1)
+				output += "s";
+			output += ",";
+		}
+		
+		if(byId!=null) {
+			boolean b = Boolean.parseBoolean(byId);
+			this.authors.rankedById(b);
+		}
+		else {
+			this.authors.rankedByHits(byHits);
+		}
 		this.authors.take(take);
 		this.authors = this.query.answer();
 		
 		List<Long> result = new ArrayList<Long>();
 		for(Author a : this.authors.getAuthors())
 			result.add(a.getId());
-		String output = "Users that mentioned " +
-		"at least: " + tm_al +
-		" when: " + tm_when +
-		" amongst: " + tm_users +
-		" and who follow " +
-		"at least: " + wf_al +
-		" amongst: " + wf_users +
-		" ----> " + result;
-		return Response.status(200).entity(output).build();
+		String qAndA = output.substring(0, output.length()-1);
+		qAndA += " ----> " + result;
+		return Response.status(200).entity(qAndA).build();
 	}
 	
 	@GET
 	@Path("/users/thatMentioned")
 	public Response usersThatMentioned(
-		@QueryParam("atLeast") int tm_al,
+		@DefaultValue("1") @QueryParam("atLeast") int tm_al,
 		@QueryParam("when") String tm_when,
 		@QueryParam("users") String tm_users,
 		@DefaultValue("true") @QueryParam("byId") boolean byId,
@@ -86,18 +129,20 @@ public class RestManager {
 		List<Long> result = new ArrayList<Long>();
 		for(Author a : this.authors.getAuthors())
 			result.add(a.getId());
-		String output = "Users that mentioned: " +
-		"at least: " + tm_al +
-		" when: " + tm_when +
-		" amongst: " + tm_users +
-		" ----> " + result;
+		String output = "Users that mentioned" +
+						" at least: " + tm_al + " user";
+		if(tm_al>1)
+			output += "s";
+		output += " when: " + tm_when +
+				" amongst: " + tm_users +
+				" ----> " + result;
 		return Response.status(200).entity(output).build();
 	}
 	
 	@GET
 	@Path("/users/whoFollow")
 	public Response usersWhoFollow(
-		@QueryParam("atLeast") int wf_al,
+		@DefaultValue("1") @QueryParam("atLeast") int wf_al,
 		@QueryParam("users") String wf_users,
 		@DefaultValue("true") @QueryParam("byId") boolean byId,
 		@DefaultValue("10") @QueryParam("take") int take){
@@ -112,10 +157,12 @@ public class RestManager {
 		List<Long> result = new ArrayList<Long>();
 		for(Author a : this.authors.getAuthors())
 			result.add(a.getId());
-		String output = "Users who follow: " +
-		"at least: " + wf_al +
-		" amongst: " + wf_users +
-		" ----> " + result;
+		String output = "Users who follow" +
+						" at least: " + wf_al + " user";
+		if(wf_al>1)
+			output += "s";
+		output += " amongst: " + wf_users +
+				  " ----> " + result;
 		return Response.status(200).entity(output).build();
 	}
 	
@@ -136,7 +183,7 @@ public class RestManager {
 		List<Long> result = new ArrayList<Long>();
 		for(Author a : this.authors.getAuthors())
 			result.add(a.getId());
-		String output = "Users whose followers follow " + users +
+		String output = "Users whose followers follow one amongst " + users +
 		" ----> " + result;
 		return Response.status(200).entity(output).build();
 	}
@@ -158,7 +205,7 @@ public class RestManager {
 		List<Long> result = new ArrayList<Long>();
 		for(Author a : this.authors.getAuthors())
 			result.add(a.getId());
-		String output = "Users whose followers are followed by " + users +
+		String output = "Users whose followers are followed by one amongst" + users +
 		" ----> " + result;
 		return Response.status(200).entity(output).build();
 	}
@@ -167,8 +214,8 @@ public class RestManager {
 	@Path("/users/whoseFollowersMentioned")
 	public Response usersWhoseFollowersMentioned(
 		@QueryParam("when") String when,
-		@QueryParam("atLeast") int al,
-		@QueryParam("minTimes") int minTimes,
+		@DefaultValue("1") @QueryParam("atLeast") int al,
+		@DefaultValue("1") @QueryParam("minTimes") int minTimes,
 		@QueryParam("users") String users,
 		@DefaultValue("true") @QueryParam("byHits") boolean byHits,
 		@DefaultValue("10") @QueryParam("take") int take){
@@ -183,8 +230,16 @@ public class RestManager {
 		List<Long> result = new ArrayList<Long>();
 		for(Author a : this.authors.getAuthors())
 			result.add(a.getId());
-		String output = "Users whose followers are followed by " + users +
-		" ----> " + result;
+		String output = "Users whose followers mentioned " + 
+						"at least: " + al + " user";
+		if(al>1)
+			output += "s";
+		output += " when: " + when +
+					"exactly or more than " + minTimes + " time";
+		if(minTimes>1)
+			output += "s";
+		output += " amongst: " + users +
+				  " ----> " + result;
 		return Response.status(200).entity(output).build();
 	}
 	
